177b0d4d9705579d0738d5b7e5591c2bcec9a54d
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-01-26 19:28:24 -0300

    Adicionando componentes no html com for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,5 @@ import { SeparadorComponent } from "./componentes/separador/separador.component"
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
