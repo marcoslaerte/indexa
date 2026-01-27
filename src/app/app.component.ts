a8098015b09576d80f5cb3828ee7d32a9737982a
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-01-27 19:42:46 -0300

    Realizando filtro por texto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { ContainerComponent } from "./componentes/container/container.component"
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { SeparadorComponent } from "./componentes/separador/separador.component";
 import { ContatoComponent } from "./componentes/contato/contato.component";
+import { FormsModule } from '@angular/forms';
 
 interface Contato {
   id: number,
@@ -23,7 +24,8 @@ import agenda from './agenda.json'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -32,8 +34,19 @@ export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
   contatos: Contato[] = agenda;
 
-  filtrarContatosPelaLetraInical(letra: string): Contato[] {
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contato[] {
+    if(!this.filtroPorTexto) {
+      return this.contatos;
+    }
     return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatosPelaLetraInical(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter(contato => {
       return contato.nome.toLowerCase().startsWith(letra);
     });
   }
