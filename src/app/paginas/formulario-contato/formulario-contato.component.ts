14241a1913b0978f222211eb43d9bfe7b3112c83
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2026-01-29 17:25:05 -0300

    Adicionando FormGroup e formControlName para realizar o bind dos campos no componente para o template

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../componentes/container/container.component";
 import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
   imports: [
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Nay'),
+      telefone: new FormControl('99 9999-9999'),
+      email: new FormControl('nay@email.com'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('Olá Mundo!')
+    });
+  }
 
 }
